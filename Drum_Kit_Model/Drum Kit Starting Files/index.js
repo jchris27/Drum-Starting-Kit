119c0ee01554ad159f61374a6e7cac32b730e843
Jin <jchris1227@icloud.com>
2019-09-09 21:12:53 +0900

    initialize drum kit files

---
+++ Drum_Kit_Model/Drum Kit Starting Files/index.js
@@ -0,0 +1,85 @@
+// document.querySelector(".set").addEventListener("click", function () {
+//   alert("I got clicked!");
+//
+// });
+
+
+// Another method using for loop and querySelectorAll //
+
+// Detecting Button Press
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrumButtons; i++) {
+
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    // What to do when click detected.
+
+    var buttonInnerHTML = this.innerHTML;
+
+    makeSoundKeys(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
+
+  });
+}
+
+// Detecting Keyboard Press
+
+document.addEventListener("keydown", function(evt) {
+  makeSoundKeys(evt.key);
+  buttonAnimation(evt.key);
+});
+
+function makeSoundKeys(keyPress) {
+
+  switch (keyPress) {
+    case "w":
+      var tom1 = new Audio('sounds/tom-1.mp3');
+      tom1.play();
+      break;
+
+    case "a":
+      var tom2 = new Audio('sounds/tom-2.mp3');
+      tom2.play();
+      break;
+
+    case "s":
+      var tom3 = new Audio('sounds/tom-3.mp3');
+      tom3.play();
+      break;
+
+    case "d":
+      var snare = new Audio('sounds/snare.mp3');
+      snare.play();
+      break;
+
+    case "j":
+      var crash = new Audio('sounds/crash.mp3');
+      crash.play();
+      break;
+
+    case "k":
+      var kick = new Audio('sounds/kick-bass.mp3');
+      kick.play();
+      break;
+
+    case "l":
+      var tom4 = new Audio('sounds/tom-4.mp3');
+      tom4.play();
+      break;
+
+    default:
+      console.log();
+  }
+}
+
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function() {
+    activeButton.classList.remove("pressed");
+  }, 100);
+  setTimeout(function() {
+    document.querySelector("." + currentKey).classList.toggle("colorChanger");
+  }, 100);
+
+}
